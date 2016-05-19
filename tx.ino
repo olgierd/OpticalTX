c8520e22854304f41b0d68c78526ce016a4e49e8
Olgierd <opilarczyk@gmail.com>
2016-05-19 21:36:52 +0200

    added standalone mode + full configuration via serial port

--- tx.ino
+++ tx.ino
@@ -1,63 +1,61 @@
 #include <JTEncode.h>
 
-#define JT9                   // transmission mode [ JT9 / JT65 / CW / PSK ]
-#define BASEFREQ 2620         // frequency [hz]
+
+#define MODE_JT65 1
+#define MODE_JT9 2
+#define MODE_PSK 3
+#define MODE_CW 4
+
 #define SIGNAL_PIN 4          // arduino output pin
 
-#define JT_SERIALSYNC 0       // when enabled, starts transmission in JT modes after receiving any char on serial input
+// when "mode" is set to value other than 0, code runs in non-interactive mode
+uint8_t mode=MODE_PSK;
+// frequency
+int BASEFREQ=1600;
+uint8_t WPMSPEED = 20;
+uint8_t messagelen=8;
+char message[50] = "KANAPKA ";
 
-#define WPMSPEED 20
-
-char message[] = "HELLO DR OM";
+uint8_t txloop=1;
 
 
 
 
 
-// don't touch this part, unless you know what you're doing //////////////////////////////////////////////////////
-#ifdef JT65
-#define SYMBOLTIME 372
-#define SYMLENGTH 126
-#define SYMTIME 2.6917
-#define DELAYAFTER 13150
-#endif
+#define JT65_SYMBOLTIME 372
+#define JT65_SYMLENGTH 126
+#define JT65_SYMSHIFT 2.6917
+#define JT65_DELAYAFTER 13150
 
-#ifdef JT9
-#define SYMBOLTIME 580
-#define SYMLENGTH 85
-#define SYMTIME 1.736
-#define DELAYAFTER 10750
-#endif
+#define JT9_SYMBOLTIME 580
+#define JT9_SYMLENGTH 85
+#define JT9_SYMSHIFT 1.736
+#define JT9_DELAYAFTER 10900
 
-#ifdef PSK
-#define SYMBOLTIME 32
-uint16_t varicode[] = { 1, 1, 887, 747, 863, 751, 765, 767, 239, 29, 31, 733, 31, 885, 939, 759, 1, 757, 941, 943, 859, 875, 877, 855, 891, 893, 951, 853, 861, 955, 763, 895, 1, 511, 351, 501, 475, 725, 699, 383, 251, 247, 367, 479, 117, 53, 87, 431, 183, 189, 237, 255, 375, 347, 363, 429, 427, 439, 245, 445, 493, 85, 471, 687, 701, 125, 235, 173, 181, 119, 219, 253, 341, 127, 509, 381, 215, 187, 221, 171, 213, 477, 175, 111, 109, 343, 437, 349, 373, 379, 685, 503, 495, 507, 703, 365, 735, 11, 95, 47, 45, 3, 61, 91, 43, 13, 491, 191, 27, 59, 15, 7, 63, 447, 21, 23, 5, 55, 123, 107, 223, 93, 469, 695, 443, 693, 727, 949 };
-#endif
+#define PSK_SYMBOLTIME 32
 
-#ifdef CW
-unsigned char morse[] = { 33,104,106,68,0,98,70,96,32,103,69,100,35,34,71,102,109,66,64,1,65,97,67,105,107,108,18,159,143,135,131,129,128,144,152,156,158 };
-#define DITTIME 1200.0/WPMSPEED
-#define DAHTIME 3*DITTIME-5
 #define CWSHIFT 600
-#endif
 
 #define SETFREQ(x) OCR1A = 8000000/(x)
 
+
+int SYMBOLTIME=0;
+int SYMLENGTH=0;
+double SYMSHIFT=0;
+int DELAYAFTER=0;
+
+int DITTIME, DAHTIME;
+
+uint16_t varicode[] = { 1, 1, 887, 747, 863, 751, 765, 767, 239, 29, 31, 733, 31, 885, 939, 759, 1, 757, 941, 943, 859, 875, 877, 855, 891, 893, 951, 853, 861, 955, 763, 895, 1, 511, 351, 501, 475, 725, 699, 383, 251, 247, 367, 479, 117, 53, 87, 431, 183, 189, 237, 255, 375, 347, 363, 429, 427, 439, 245, 445, 493, 85, 471, 687, 701, 125, 235, 173, 181, 119, 219, 253, 341, 127, 509, 381, 215, 187, 221, 171, 213, 477, 175, 111, 109, 343, 437, 349, 373, 379, 685, 503, 495, 507, 703, 365, 735, 11, 95, 47, 45, 3, 61, 91, 43, 13, 491, 191, 27, 59, 15, 7, 63, 447, 21, 23, 5, 55, 123, 107, 223, 93, 469, 695, 443, 693, 727, 949 };
+
+unsigned char morse[] = { 33,104,106,68,0,98,70,96,32,103,69,100,35,34,71,102,109,66,64,1,65,97,67,105,107,108,18,159,143,135,131,129,128,144,152,156,158 };
+
 int x=0, y=0, t=0, key=0, val=0, len=0;
-uint8_t symbols[255];
+uint8_t symbols[130];
 JTEncode jtencode;
 
 void setup() {
   pinMode(SIGNAL_PIN, OUTPUT);     
-
-  #ifdef JT65
-    jtencode.jt65_encode(message, symbols);
-  #endif
-
-  #ifdef JT9
-    jtencode.jt9_encode(message, symbols);
-  #endif
-
 
   noInterrupts();           // disable  interrupts
   TCCR1A = 0;
@@ -76,44 +74,42 @@ void setup() {
 
 ISR(TIMER1_COMPA_vect)          // timer interrupt service
 {
-  digitalWrite(4, t);
-  digitalWrite(5, t);
+  digitalWrite(SIGNAL_PIN, t);
   t=1-t;
 }
 
 
 
-#if defined(JT65) || defined(JT9)
-void tx() {
+void tx_jt() {
   
   
   while(1) {
-    if(JT_SERIALSYNC) { while(!Serial.available()) {} }
   
     while(1) {
-      SETFREQ((BASEFREQ+symbols[x]*SYMTIME)+0.5);  // set appropriate frequency
+      Serial.println(symbols[x]);
+      SETFREQ((BASEFREQ+symbols[x]*SYMSHIFT)+0.5);  // set appropriate frequency
       x++;
       delay(SYMBOLTIME);    // wait until symbol is transmitted
       if(x==SYMLENGTH) {    // after transmitting all symbols
         x=0;
-        SETFREQ(500);         // fill the gap between transmissions with 2kHz constant tone
-        if(JT_SERIALSYNC) break;
+        SETFREQ(BASEFREQ-500);         // fill the gap between transmissions with 2kHz constant tone
+        if(!txloop) return;
         delay(DELAYAFTER);
       }
+      if(Serial.read() == 'q') { return; }
     }
     
-    if(JT_SERIALSYNC) { while(Serial.available()) {Serial.read();} }
   }
   
 }
-#endif
 
-#if defined(PSK)
-short cur_sym;
-void tx() {
+
+void tx_psk() {
+  short cur_sym;
   SETFREQ(BASEFREQ);
+  
   while(1) {
-    for(x=0; x<sizeof(message); x++) {  
+    for(x=0; x<messagelen; x++) {  
       cur_sym = varicode[message[x]];
       y = 15;
       while((cur_sym & 1<<y) == 0) y--;
@@ -123,25 +119,31 @@ void tx() {
          if((cur_sym & (1<<y)) == 0) t = 1-t;    // if the current bit to transmit == 0 - invert signal phase
          delay(SYMBOLTIME);
       }
+      
     }
+    if(Serial.read() == 'q' | !txloop) { return; }
   }
 }
-#endif
 
-#if defined(CW)
-void tx() {
+void tx_cw() {
+  uint8_t ena=1;
   
-    for(x=0; x<sizeof(message)/sizeof(char); x++) {   // for every symbol
+  while(1) {
+    
+    for(x=0; x<messagelen; x++) {   // for every symbol
+      
       key = message[x];
-
+      Serial.println(key);
       // space
-      if(key == ' ') delay(DAHTIME);
+      if(key == ' ') { delay(DAHTIME); continue; }
 
       // letters
-      if(key >= 'A' && key <= 'Z') { val = morse[key-'A']; len = ((morse[key-'A'])>>5) + 1; }
-      if(key >= 'a' && key <= 'z') { val = morse[key-'a']; len = ((morse[key-'a'])>>5) + 1; }
+      if(key >= 'A' && key <= 'Z') { val = morse[key-'A'];  }
+      if(key >= 'a' && key <= 'z') { val = morse[key-'a'];  }
 
-      // numbers
+      len = (val>>5) + 1;
+
+      // numbers and /
       if(key >= '/' && key <= '9') { val = morse[key-'0'+27]; len = 5; }
 
       // chars
@@ -157,16 +159,122 @@ void tx() {
         len--; 
       }
       delay(DAHTIME);   // dah time - spacing between letters
-      
-    }
+
+      if(Serial.read() == 'q' || !txloop) { return; }
+
+   }
+ }
   
 }
-#endif
+
+
+int serialInt() {
+  int ret = 0;
+  char c;
+  while(1) {
+    while(!Serial.available()) {}
+    c = Serial.read();
+    if(!(c>='0' && c<='9')) {
+      return ret;
+    }
+    else {
+      ret = ret*10 + c-'0';
+    }
+  }
+}
+
+void readSerialMessage() {
+  
+  uint8_t x=0;
+  char c;
+
+  while(1) {
+    while(!Serial.available()) {}
+    c = Serial.read();
+    if(c == '\n' || c == '\r') {
+      message[x] = 0x00;
+      messagelen =  x;
+      return;
+    }
+    else {
+      message[x] = c;
+      x++;
+    }
+    
+  }
+  
+}
+
+
+
 
 void loop() {
 
+  Serial.println("mode/freq/wpm/loop/msg");
 
-    tx();
+  if(mode == 0) {
+    mode = serialInt();
+    BASEFREQ = serialInt();
+    WPMSPEED = serialInt();
+    txloop = serialInt();
+    readSerialMessage();
+  }
+
+  switch(mode) {
+    case MODE_JT65:
+    {
+      SYMBOLTIME = JT65_SYMBOLTIME;
+      SYMLENGTH = JT65_SYMLENGTH;
+      SYMSHIFT = JT65_SYMSHIFT;
+      DELAYAFTER = JT65_DELAYAFTER;
+      
+      jtencode.jt65_encode(message, symbols);
+
+      tx_jt();
+      break;
+    }
+    case MODE_JT9:
+    {
+      SYMBOLTIME = JT9_SYMBOLTIME;
+      SYMLENGTH = JT9_SYMLENGTH;
+      SYMSHIFT = JT9_SYMSHIFT;
+      DELAYAFTER = JT9_DELAYAFTER;
+
+      jtencode.jt9_encode(message, symbols);
+
+      tx_jt();
+      break;
+    }
+    case MODE_PSK:
+    {
+      SYMBOLTIME = PSK_SYMBOLTIME;
+
+      message[messagelen] = '\n';
+      messagelen++;
+
+      tx_psk();
+      
+      break;
+    }
+    case MODE_CW:
+    {
+
+      DITTIME = 1200.0/WPMSPEED;
+      DAHTIME = 3*DITTIME-5;
+
+      message[messagelen] = ' ';
+      messagelen++;
+      
+      tx_cw();
+      break;
+    }
+    
+  }
+
+
+  asm volatile ("  jmp 0");  
+
+  
 }
 
 
